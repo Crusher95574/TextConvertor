947dfe195b3489439b115a98cad80995d4a4f3fe
DIVAY KANSAL <divaykansal@DIVAYs-MacBook-Air.local>
2023-01-04 19:27:09 +0530

    Vedio 11 complted (Dark mode enabled

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,14 +2,26 @@ import React,{useState} from 'react'
 
 export default function TextForm(props) {
   const handleUpClick=()=>{
-  //  console.log("Uppercase was clicked"+text);
     let newText=text.toUpperCase();
     setText(newText);
   }
+  const handleLowClick=()=>{
+    let newText=text.toLowerCase();
+    setText(newText);
+  }
+ 
+  const handleClearClick=()=>{
+    let newText=" ";
+    setText(newText);
+  }
+  const handleCapitalClick=()=>{
+    let newText=text.toLowerCase().split(' ').map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');;
+    setText(newText);
+  }
   const handleOnChange=(event)=>{
-  //  console.log("On change");
     setText(event.target.value);
   }
+
     const [text,setText]= useState('Enter the text');
 
 
@@ -20,14 +32,20 @@ export default function TextForm(props) {
 <div className="mb-3">
   <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 </div>
-<button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-    </div>
+<button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+<button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>  
+<button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear</button>  
+<button className="btn btn-primary mx-2" onClick={handleCapitalClick}>Capitalize</button>  
+ </div>
 
     <div className="container my-3">
       <h1>Your text summary</h1>
-      <p>23334 words abd 456776 characters</p>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+     <p>{0.008*text.split(" ").length } Minutes read</p>
+     <h>Preview</h>
+     <p>{text}</p>
     </div>
 
 </>
-  )
+  ) 
 }
